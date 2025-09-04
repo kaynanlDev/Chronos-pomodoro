599a058d610aac5ead27f60bb5c2001d3284398b
kaynanDev <kaynan2817@gmail.com>
2025-09-04 12:48:49 -0300

    Criação e estilização de componente Menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import './style/theme.css';
 import './style/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
       </Container>
     </>
   );
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,20 @@
+import { HouseIcon, SettingsIcon, SunIcon, TimerIcon } from 'lucide-react';
+import styles from './styles.module.css';
+export function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <a href='#' className={styles.menuLink}>
+        <HouseIcon />
+      </a>
+      <a href='#' className={styles.menuLink}>
+        <TimerIcon />
+      </a>
+      <a href='#' className={styles.menuLink}>
+        <SettingsIcon />
+      </a>
+      <a href='#' className={styles.menuLink}>
+        <SunIcon />
+      </a>
+    </nav>
+  );
+}
