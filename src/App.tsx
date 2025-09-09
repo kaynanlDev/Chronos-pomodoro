c4ed91e2172daf7a54ed4803ceb9f7651c05bb06
kaynanDev <kaynan2817@gmail.com>
2025-09-08 22:47:12 -0300

    finalização do CountDown e estrutura do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './style/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
@@ -13,6 +14,30 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <label htmlFor='input'>text</label>
+            <input type='text' name='input' id='input' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
